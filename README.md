afac3f421a3f57258d39ab3eefdca3e9bcc0607b
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-01-05 15:46:37 +0900

    オッ

---
+++ README.md
@@ -0,0 +1,21 @@
+# オッ
+
+## Usage
+
+```
+$ o
+```
+
+## Installation
+
+```
+go get github.com/mattn/o
+```
+
+## License
+
+MIT
+
+## Author
+
+Yasuhiro Matsumoto (a.k.a. mattn)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"regexp"
+	"time"
+
+	rss "github.com/jteeuwen/go-pkg-rss"
+	"github.com/mattn/go-gntp"
+)
+
+var (
+	re = regexp.MustCompile(`^オッ\s*(https?://\S+)?`)
+)
+
+func main() {
+	client := gntp.NewClient()
+	client.AppName = "オッ"
+	client.Register([]gntp.Notification{
+		{Event: "オッ", Enabled: true},
+	})
+
+	for {
+		err := rss.New(5, true, nil,
+			func(feed *rss.Feed, ch *rss.Channel, newitems []*rss.Item) {
+				for _, item := range newitems {
+					m := re.FindAllStringSubmatch(item.Title, -1)
+					if len(m) == 0 {
+						continue
+					}
+
+					client.Notify(&gntp.Message{
+						Event:    "オッ",
+						Title:    "オッRSS",
+						Text:     item.Title,
+						Icon:     "https://pbs.twimg.com/profile_images/1555680104/todesking.png",
+						Callback: m[0][1],
+					})
+				}
+			},
+		).Fetch(`https://queryfeed.net/twitter?q=from%3Atodesking&title-type=tweet-text-full`, nil)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		time.Sleep(30 * time.Second)
+	}
+}
